88c96004dd836f4bf79ddbf26d470fc127cd43c9
gerardo martijena <martijena1@gmail.com>
2022-07-28 18:26:49 -0300

    fech

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 let carrito=[];
 let total=0;
-
+let listaProductos=[];
 const cardsCarrito=()=>{
     const carritoVacio=()=>{
 
@@ -17,12 +17,12 @@ const cardsCarrito=()=>{
         const restarCantidad=(btnRestarId) =>{
             const btnRestar=document.getElementById(`btnRestar${btnRestarId}`);
                 btnRestar.addEventListener('click', () => {
-                    const producto = articulos.find((producto) => producto.id === btnRestarId);
+                    const producto = listaProductos.find((producto) => producto.id === btnRestarId);
                     const item = carrito.find((productoCarrito) => productoCarrito.id === btnRestarId);
                     if(item.cantidad>1){
                         item.cantidad--;
                         producto.stock++;
-                        articulos.push(producto);
+                        listaProductos.push(producto);
                         calcularTotal();
                         cardsCarrito();
                         Toastify({
@@ -44,7 +44,7 @@ const cardsCarrito=()=>{
                                 const indice=carrito.indexOf(item)
                                 carrito.splice(indice,1);
                                 producto.stock++;
-                                articulos.push(producto);
+                                listaProductos.push(producto);
                                 calcularTotal();
                                 cardsCarrito();
                                 swal.fire({
@@ -69,12 +69,12 @@ const cardsCarrito=()=>{
                     cancelButtonText:`cancelar`
                 }).then((result)=>{
                     if(result.isConfirmed){
-                        const producto = articulos.find((producto)=>producto.id === btnEliminarId);
+                        const producto = listaProductos.find((producto)=>producto.id === btnEliminarId);
                         const item=carrito.find((producto)=>producto.id===btnEliminarId);
                         const indice=carrito.indexOf(item)
                         carrito.splice(indice,1);
                         producto.stock+=item.cantidad;
-                        articulos.push(producto);
+                        listaProductos.push(producto);
                         calcularTotal();
                         cardsCarrito();
                     }
@@ -92,7 +92,7 @@ const cardsCarrito=()=>{
                     cancelButtonText:`cancelar`
                 }).then((result)=>{
                     carrito.forEach(productoCarrito => {
-                        let producto = articulos.find((producto)=>producto.id === productoCarrito.id);
+                        let producto = listaProductos.find((producto)=>producto.id === productoCarrito.id);
                         producto.stock = productoCarrito.cantidad;
                     });
                     carrito.length=0;
@@ -173,7 +173,7 @@ const agregarAlCarro=(id)=>{
         carrito.push(productoNuevo);
     };
 
-    let producto=articulos.find(articulos=>articulos.id===id);
+    let producto=listaProductos.find(articulos=>articulos.id===id);
     let productoEnCarro=carrito.find(articulos=>articulos.id===id);
     if(producto.stock==0){
         swal.fire({
@@ -188,7 +188,7 @@ const agregarAlCarro=(id)=>{
     else{
         productoEnCarro ? productoEnCarro.cantidad++ : pushNuevoProduto();
         producto.stock--;
-        articulos.push(producto);
+        listaProductos.push(producto);
         Toastify({
             text:`${producto.name} Agregado `,
             duration:2000,
@@ -289,5 +289,9 @@ const verCarritosGuardados=()=>{
         };
     });
 }
-
-mostrarProductos(articulos);
+fetch("src/data/productos.json")
+    .then((response) => response.json())
+    .then((articulos)=>{
+        listaProductos=articulos;
+        mostrarProductos(listaProductos);
+    })
